a8ccac536dd8dff49adc74cc52684a8bae84e6c3
AlnisS <alnis.smidchens@gmail.com>
2022-05-01 02:05:49 -0700

    Audio sync with animations

--- main.gd
+++ main.gd
@@ -12,7 +12,11 @@ func _ready():
 func _physics_process(delta):
 	$Player/CameraBase.rotation_degrees.y = -$Player.velocity.x * 2
 	$Player/CameraBase.rotation_degrees.x = -$Player.velocity.z * 2
-#	$InfoLabel.text = str($Player/CoyoteTime.time_left)
+	
+	if Input.is_action_just_pressed("skip"):
+		_on_level_finished()
+	
+	$InfoLabel.text = str($Player/CoyoteTime.time_left)
 
 func reset_player():
 	$Player.translation = Vector3(0, 0.5, 0)
@@ -26,6 +30,28 @@ func switch_to_level(index):
 	new_level.connect("level_finished", self, "_on_level_finished")
 	$LevelNumber.text = str(level_index + 1)
 	reset_player()
+	get_tree().set_group("music_sync_animation", "playback_speed", 1.166667)
+	
+	# animations seem to not immediately start on scene switch
+	# waiting 2 frames seems to be enough to sync on all platforms
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+
+	var music_playhead = fmod(get_music_playhead(), 1.0)
+	for animation_player in get_tree().get_nodes_in_group("music_sync_animation"):
+		var animation_playhead = fmod(animation_player.current_animation_position, 1.0)
+		animation_player.advance(music_playhead - animation_playhead)
+
+
+# music playhead position as a fraction of a beat
+func get_music_playhead():
+	var music_time = $Music.get_playback_position()
+	music_time += AudioServer.get_time_since_last_mix()
+	music_time -= AudioServer.get_output_latency()
+	# theoretically there should be a delay of 0.03 seconds because the music
+	# doesn't immediately start in the mp3... but it sounds better without it?
+#	music_time -= 0.03
+	return music_time * 1.16666667
 
 
 func _on_Player_player_died():
